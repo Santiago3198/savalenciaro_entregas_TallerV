e770dde95704ea4d8ad983e7b96046018a09f146
santiago3198 <savalenciaro@unal.edu.co>
2023-03-07 09:40:05 -0500

    Correccion

--- DesarrolloHal/Inc/stm31fxxhal.h
+++ DesarrolloHal/Inc/stm31fxxhal.h
@@ -86,7 +86,7 @@ typedef struct
 	volatile uint32_t PLLI2SCFGR;
 	volatile uint32_t reserved14;
 	volatile uint32_t DCKCFGR;
-} RCC RedDef t;        //¿Por que no cambia de color a azul?.
+} RCC_RedDef_t;        //¿Por que no cambia de color a azul?.
 
 #define RCC		((RCC_RegDef_t *) RCC_BASE_ADDR)
 
