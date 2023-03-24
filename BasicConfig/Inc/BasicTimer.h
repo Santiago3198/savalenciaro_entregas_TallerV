57605fb9615e09787dd39a58dd6d7d077fb38f01
Santiago3198 <126313440+Santiago3198@users.noreply.github.com>
2023-03-23 22:44:16 -0500

    BasicTimer

--- BasicConfig/Inc/BasicTimer.h
+++ BasicConfig/Inc/BasicTimer.h
@@ -10,20 +10,20 @@
 
 #include "stm32f4xx.h"
 
-#define BTIMER_MODE_UP		0
-#define BTIMER_MODE_DOWN	1
+#define BTIMER_MODE_UP				0
+#define BTIMER_MODE_DOWN			1
 
-#define BTIMER_SPEED_10us		160
-#define BTIMER_SPEED_100us		1600
+#define BTIMER_SPEED_10us			160
+#define BTIMER_SPEED_100us			1600
 #define BTIMER_SPEED_16000us		16000
 
 /*Estructura que contiene la configutacion minima necesaria para el manejo del timer*/
 
 typedef struct
 {
-	uint8_t TIMx_mode;		//Up or Down
-	uint32_t TIMx_speed;		//A qué velocidad se incrementa el timer
-	uint32_t TIMx_period;		//Valor en ms del periodo del timer
+	uint8_t TIMx_mode;				//Up or Down
+	uint32_t TIMx_speed;			//A qué velocidad se incrementa el timer
+	uint32_t TIMx_period;			//Valor en ms del periodo del timer
 	uint8_t	TIMx_interruptEnable;	//Activa o desactiva el modo interrupcion del timer
 
 }BasicTimer_Config_t;
@@ -38,7 +38,7 @@ typedef struct
 }BasicTimer_Handler_t;
 
 void BasicTmer_Config(BasicTimer_Handler_t *ptrBTimerHandler);
-void BasicTimerX_Callback(void); //Esta funcion debe ser sobreescrita en el main para que el sistema funcione
+void BasicTimerX_Callback(void); 		//Esta funcion debe ser sobreescrita en el main para que el sistema funcione
 
 
 
--- BasicConfig/Src/BasicTimer.c
+++ BasicConfig/Src/BasicTimer.c
@@ -31,19 +31,25 @@ TIM_TypeDef		*ptrTimerUsed;
  */
 
 void BasicTimer_Config(BasicTimer_Handler_t *ptrBTimerHandler){
+
 	//Guardamos una referencia al periferico que estamos utilizando
+
 	ptrTimerUsed = ptrBTimerHandler->ptrTIMx;
 
 	/*0. Desactivamos las interrupciones globales mientras configuramos el sistema.*/
+
 	__disable_irq();
 
 	/*1.Activar la señal de reloj del periferico requerido*/
+
 	if(ptrBTimerHandler->ptrTIMx == TIM2){
+
 		//Registro del RCC que nos activa la señal de reloj para el TIM2
 
 	}
 	else if(ptrBTimerHandler->ptrTIMx == TIM3){
-		//Registro del RCC que nos activa la señal de reloj para el TIM2
+
+		//Registro del RCC que nos activa la señal de reloj para el TIM3
 
 	}
 	else{
@@ -69,13 +75,87 @@ void BasicTimer_Config(BasicTimer_Handler_t *ptrBTimerHandler){
 		/*ESCRIBIR CODIGO*/
 
 		/*3b. Configuramos el Autoreload. Este es el "limite" hasta donde el CNT va a contar */
+
 		ptrBTimerHandler->ptrTIMx->ARR = ptrBTimerHandler->TIMx_Config.TIMx_period - 1;
 
 		/*3c. Reiniciamos el registro counter*/
 
 		/*ESCRIBIR CODIGO*/
+
+	}else{
+		/*3a. Estamos en DOWN_Mode, el limite se carga en ARR (0) y se comienza con un valor alto
+		 * Trabaja contando en direccion descendente
+		 */
+		/*ESCRIBIR CODIGO*/
+
+		/*3b. Configuramos el autoreload. Este es el limite hasta donde el CNT va a contar
+		 * En modo descendente, con numeros positivos, cual es el minimo valor que el ARR puede tomar*/
+
+		 /*ESCRIBIR CODIGO*/
+
+		/*3c. Reiniciamos el registro counter
+		 * Este es el valor con el que el counter comienza */
+
+		ptrBTimerHandler->ptrTIMx->CNT = ptrBTimerHandler->TIMx_Config.TIMx_period - 1;
+
 	}
 
+	/*4. Activamos el Timer (el CNT debe comenzar a contar */
 
+	ptrBTimerHandler->ptrTIMx->CR1 |= TIM_CR1_CEN;
 
+	/*5. Activamos la interrupcion debida al Timerx Utilizado
+	 * Modificar el registro encargado de activar la interrupcion generada por el TIMx */
+
+	/*ESCRIBIR CODIGO*/
+
+	/*6. Activamos el canal del sistema NVIC para que lea la interrupcion */
+
+	if (ptrBTimerHandler->ptrTIMx == TIM2){
+
+		//Activando el NVIC para la interrupcion del TIM2
+
+		NVIC_EnableIRQ(TIM2_IRQn);
+
+	}
+	else if (ptrBTimerHandler->ptrTIMx == TIM3){
+
+		//Activando el NVIC para la interrupcion del TIM3
+
+		/*ESCRIBIR CODIGO*/
+
+	}
+	else{
+		__NOP();
+	}
+
+	/*7. Volvemos a activar las interrupciones del sistema*/
+
+	__enable_irq();
+
+}
+
+__attribute__((weak)) void BasicTimerX_Callback(void){
+
+	/*NOTE: This function should not be modified, when the callback id needed
+	 * the BasicTimerX_Callback could be implemented in the main file */
+
+	__NOP();
+
+}
+
+/*Esta es la funcion a la que apunta el ssema en el vector de interrupciones.
+ * Se debe utilizar usando el mismo nombre definido en el vector de interrupciones.
+ * Al hacerlo correctamente, el sistema apunta a esta funcion y cuando la interrupcion se lanza
+ * el sistema inmediatamente salta a este lugar en la memoria */
+
+void TIM2_IRQHandler(void){
+
+	/*Limpiamos la bandera que indica que la interrupcion se ha generado */
+
+	ptrTimerUsed->SR &= ÑÑTIM_SR_UIF;
+
+	/*Lamamos a la funcion que se debe encargar de hacer algo con esta interrupcion */
+
+	BasicTimerX_Callback();
 }
