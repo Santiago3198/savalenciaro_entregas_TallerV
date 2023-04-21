751b09da7b7db4b35218e72d0880cd9047ddbd41
santiago3198 <savalenciaro@unal.edu.co>
2023-04-21 10:59:29 -0500

    Corrección USART

--- BasicConfig/Drivers/Src/USARTxDriver.c
+++ BasicConfig/Drivers/Src/USARTxDriver.c
@@ -76,32 +76,30 @@ void USART_Config(USART_Handler_t *ptrUsartHandler){
 	switch(ptrUsartHandler->USART_Config.USART_stopbits){
 	case USART_STOPBIT_1: {
 		// Debemoscargar el valor 0b00 en los dos bits de STOP
-		ptrUsartHandler->ptrUSARTx->CR2 &= ~(0b1 << 12);
-		ptrUsartHandler->ptrUSARTx->CR2 &= ~(0b1 << 13);
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~(USART_CR2_STOP);
 		break;
 	}
 	case USART_STOPBIT_0_5: {
 		// Debemoscargar el valor 0b01 en los dos bits de STOP
-		ptrUsartHandler->ptrUSARTx->CR2 &= ~(0b1 << 13);
-		ptrUsartHandler->ptrUSARTx->CR2 |= (0b1 << 12);
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~(USART_CR2_STOP);
+		ptrUsartHandler->ptrUSARTx->CR2 |= (USART_CR2_STOP_0);
 		break;
 	}
 	case USART_STOPBIT_2: {
 		// Debemoscargar el valor 0b10 en los dos bits de STOP
-		ptrUsartHandler->ptrUSARTx->CR2 &= ~(0b1 << 12);
-		ptrUsartHandler->ptrUSARTx->CR2 |= (0b1 << 13);
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~(USART_CR2_STOP);
+		ptrUsartHandler->ptrUSARTx->CR2 |= (USART_CR2_STOP_1);
 		break;
 	}
 	case USART_STOPBIT_1_5: {
 		// Debemoscargar el valor 0b11 en los dos bits de STOP
-		ptrUsartHandler->ptrUSARTx->CR2 |= (0b1 << 12);
-		ptrUsartHandler->ptrUSARTx->CR2 |= (0b1 << 13);
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~(USART_CR2_STOP);
+		ptrUsartHandler->ptrUSARTx->CR2 |= (USART_CR2_STOP);
 		break;
 	}
 	default: {
 		// En el casopor defecto seleccionamos 1 bit de parada
-		ptrUsartHandler->ptrUSARTx->CR2 &= ~(0b1 << 12);
-		ptrUsartHandler->ptrUSARTx->CR2 &= ~(0b1 << 13);
+		ptrUsartHandler->ptrUSARTx->CR2 &= ~(USART_CR2_STOP);
 		break;
 	}
 	}
