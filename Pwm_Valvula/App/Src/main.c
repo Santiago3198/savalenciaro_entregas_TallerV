0db0492ccb2b0a21cb8064dc130e145d6358cea5
Santiago3198 <savalenciaro@unal.edu.co>
2023-06-04 10:22:00 -0500

    .

--- Pwm_Valvula/App/Src/main.c
+++ Pwm_Valvula/App/Src/main.c
@@ -27,8 +27,11 @@
 
 //Definición de handlers
 GPIO_Handler_t handlerBlinky 				= {0};		//Handler Blinky
-GPIO_Handler_t handlerPinPwmChannel			= {0};		//Handler canal PWM
+GPIO_Handler_t handlerOutPin				= {0};		//Handler del pin de salida
+GPIO_Handler_t handlerUserButton			= {0};		//Handler User Button
 BasicTimer_Handler_t handlerTimerBlinky 	= {0};		//Handler Timer del blinky
+EXTI_Config_t handlerUserButtonExti 		= {0};		//Handler config Exti
+
 
 //Declaración de funciones
 void initSystem(void);
@@ -36,6 +39,8 @@ void initSystem(void);
 int main(void){
 
 	initSystem();
+
+	GPIO_WritePin(&handlerOutPin, RESET);
 
 	/*Loop forever*/
 	while(1){
@@ -62,8 +67,34 @@ void initSystem(void){
 	handlerTimerBlinky.TIMx_Config.TIMx_period 						= 2500;
 	handlerTimerBlinky.TIMx_Config.TIMx_interruptEnable 			= 1;
 	BasicTimer_Config(&handlerTimerBlinky);
+
+	//Configuración Pin de salida
+	handlerOutPin.pGPIOx 											= GPIOA;
+	handlerOutPin.GPIO_PinConfig.GPIO_PinNumber 					= PIN_0;
+	handlerOutPin.GPIO_PinConfig.GPIO_PinMode 						= GPIO_MODE_OUT;
+	handlerOutPin.GPIO_PinConfig.GPIO_PinOPType 					= GPIO_OTYPE_PUSHPULL;
+	handlerOutPin.GPIO_PinConfig.GPIO_PinSpeed 						= GPIO_OSPEEDR_FAST;
+	handlerOutPin.GPIO_PinConfig.GPIO_PinPuPdControl 				= GPIO_PUPDR_NOTHING;
+	handlerOutPin.GPIO_PinConfig.GPIO_PinAltFunMode 				= AF0;
+	GPIO_Config(&handlerOutPin);
+
+	//Configuración botón auxiliar
+	handlerUserButton.pGPIOx								= GPIOC;
+	handlerUserButton.GPIO_PinConfig.GPIO_PinNumber			= PIN_13;
+	handlerUserButton.GPIO_PinConfig.GPIO_PinMode			= GPIO_MODE_IN;
+	handlerUserButton.GPIO_PinConfig.GPIO_PinPuPdControl	= GPIO_PUPDR_NOTHING;
+	GPIO_Config(&handlerUserButton);
+
+	handlerUserButtonExti.pGPIOHandler	= &handlerUserButton; //Esto es un puntero a la estructura
+	handlerUserButtonExti.edgeType		= EXTERNAL_INTERRUPT_RISING_EDGE;
+	extInt_Config(&handlerUserButtonExti);
 }
 
 void BasicTimer2_Callback(void){
 	GPIOxTooglePin(&handlerBlinky);
 }
+
+void callback_extInt13(void){
+	GPIOxTooglePin(&handlerOutPin);
+}
+
